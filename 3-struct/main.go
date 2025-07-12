41b7e5c1ec15abd53d0c9bdc8275031da84ea5fa
Евгений <kenobi79@mail.ru>
2025-07-12 16:51:09 +0500

    Уточнение файлов проекта

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"api/binjson/bin"
+	"api/binjson/bins"
 	"fmt"
 )
 
 func main() {
-	myBin := bin.NewBin("Secret Data", true)
+	myBin := bins.NewBin("Secret Data", true)
 
 	fmt.Printf("Создан Bin:\nID: %s\nName: %s\nPrivate: %t\nCreated: %s\n",
 		myBin.ID,
@@ -15,10 +15,10 @@ func main() {
 		myBin.CreatedAt.Format("2025-01-01 15:04:05"),
 	)
 
-	binList := bin.NewBinList()
+	binList := bins.NewBinList()
 
 	binList.AddBin(myBin)
-	binList.AddBin(bin.NewBin("Public Data", false))
+	binList.AddBin(bins.NewBin("Public Data", false))
 
 	fmt.Printf("\nВ коллекции %d элементов:\n", len(binList.Bins))
 	for i, b := range binList.Bins {
